578cd2307b12ec19017a8831c0e11d2e76183baa
nedzof <nedzof@github.com>
2025-03-17 16:03:48 +0100

    cleanup

--- fix-imports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Update the BSVStats.tsx file specifically
-sed -i 's|import { API_URL } from ".*config";|import { API_URL } from "../../config";|g' src/frontend/components/charts/BSVStats.tsx
-
-echo "Import paths fixed!" 
